a2903f7ac6437736eb712b8cdcccf399946b38ef
SaraDonzellini <saradonzie@gmail.com>
2022-11-25 16:57:12 +0100

    snack 3 complete

--- js/snack_3.js
+++ js/snack_3.js
@@ -1,11 +1,17 @@
 //Calcola la somma dei primi 10 numeri di un array
+//? Calcola la somma e la media dei primi 10 numeri di un array.
 
-const ArrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 let somma = 0;
+let media = 0;
+const limite = 10;
 
-for (let i = 0; i < 10; i++) {
-  somma += ArrayNumbers[i];
-  
+for (let i = 0; i < limite; i++) {
+  somma += arrayNumbers[i];
 }
 
+media = somma / limite;
+
 console.log(somma);
+console.log(media);
+
