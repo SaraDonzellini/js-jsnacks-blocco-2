af11818c7075da9ba9d2619c18d24298f37616c0
SaraDonzellini <saradonzie@gmail.com>
2022-11-25 16:12:08 +0100

    fixes

--- js/snack_2.js
+++ js/snack_2.js
@@ -1,10 +1,10 @@
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
-const Number = parseInt(prompt("inserisci un numero"))
+const Number = parseInt(prompt("inserisci un numero"));
 
 if (Number % 2 == 0) {
-  console.log(Number)
+  console.log(Number);
 } else {
-  console.log(Number + 1)
+  console.log(Number + 1);
 }
 
