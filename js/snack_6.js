22fa3143b57b4facb405df23c26aad74a72256ef
SaraDonzellini <saradonzie@gmail.com>
2022-11-28 12:51:43 +0100

    snack 6 corrected

--- js/snack_6.js
+++ js/snack_6.js
@@ -1,11 +1,11 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 
-const arrayNumbers = [1, 22, 45, 787, 65, 43, 44, 66, 87, 68, 9, 5, 4, 23, 5, 6, 7, 8, 9, 10, 11, 12, 31]
+const arrayNumbers = [1, 22, 45, 78, 65, 43, 44, 66, 87, 68, 9, 5, 4, 23, 5, 6, 7, 8, 9, 10, 11, 12, 31]
 let somma = 0;
 
 for (let i = 0; i < arrayNumbers.length; i++) {
 
-  if (arrayNumbers.length % 2 === 1) {
+  if (i % 2 === 1) {
     somma += arrayNumbers[i]
   }
 }
